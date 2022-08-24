a48628cd0f8edcefc2f56a16c6353c5d6ea3a327
7doooddd <111942338+7doooddd@users.noreply.github.com>
2022-08-24 12:48:27 +0800

    Update JobWorker.java

--- src/main/java/ydzhao/weixin/tuisong/job/JobWorker.java
+++ src/main/java/ydzhao/weixin/tuisong/job/JobWorker.java
@@ -13,7 +13,7 @@ import ydzhao.weixin.tuisong.util.Pusher;
 @Component
 public class JobWorker {
     //要推送的用户openid
-    private static String openId = "odbd-6b1RhzrGEPCRT4rmmLKwpas";
+    private static String openId = "o3Z7p6cZ8dr8pDg3WNFR82UJjenI";
 
     @Scheduled(cron = "0 30 7 * * ?")
     public void goodMorning(){
